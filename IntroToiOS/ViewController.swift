be4b73975ebe0198b74a287701ebeb54959bf95b
SesiliTs <sesili.tsikaridze.1@btu.edu.ge>
2023-10-21 22:51:12 +0400

    Initial Commit

---
+++ IntroToiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IntroToiOS
+//
+//  Created by Sesili Tsikaridze on 21.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
